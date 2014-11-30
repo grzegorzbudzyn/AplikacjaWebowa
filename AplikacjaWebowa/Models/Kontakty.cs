506d9dd0800c1d3379a1278a1d1f8bc4bbf7c354
grzegorzbudzyn <hunterpl.grzesiu@gmail.com>
2014-11-30 15:04:15 +0100

    Revert "Revert "Nie działająca wersja""
    This reverts commit 9ea83287b32a69b5a89189b5964ed473c338e16d.

---
+++ AplikacjaWebowa/Models/Kontakty.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+namespace AplikacjaWebowa.Models
+{
+    public class Kontakty
+    {
+        public int KontaktyId { get; set; }
+        public string Nazwa { get; set; }
+        public string Adres { get; set; }
+        public string Miasto { get; set; }
+        public string Stan_Kontenera { get; set; }
+        public string Nr_Kontenera { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
